2d4834769d118bb97109ec43d73e7d95815a88be
chenqiangjun <unityboby@gmail.com>
2020-10-18 14:19:41 +0800

    Initial router

---
+++ src/views/mvs.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+  export default {
+    name: "mvs"
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/playlists.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+  export default {
+    name: "playlists"
+  }
+</script>
+
+<style scoped>
+
+</style>
